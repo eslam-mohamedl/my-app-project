7777f1c4474d2bfdbc08fa2430b6e890973074fe
Zeyadhatem391 <zeyadhatemsabry@gmail.com>
2026-03-28 21:08:16 +0200

    add feature TeamSection

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,12 @@
 import HeroSection from "@/components/organisms/HeroSection";
 import Navbar from "@/components/organisms/Navbar";
+import TeamSection from "../components/organisms/TeamSection";
 export default function page() {
   return (
     <div>
       <HeroSection />
       <Navbar />
+      <TeamSection />
     </div>
   );
 }
