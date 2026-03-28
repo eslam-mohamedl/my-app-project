c226b4c3583422670dc6624dab204ef68e5fc838
eslam-mohamedl <ee.eslam355@gmail.com>
2026-03-28 21:08:55 +0200

    add section eslam in page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,12 @@
 import HeroSection from "@/components/organisms/HeroSection";
 import Navbar from "@/components/organisms/Navbar";
+import Eslam from "@/components/organisms/Eslam";
 export default function page() {
   return (
     <div>
       <HeroSection />
       <Navbar />
+      <Eslam />
     </div>
   );
 }
