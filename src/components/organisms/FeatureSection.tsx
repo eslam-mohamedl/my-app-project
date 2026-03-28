f5330e285d3a61c8d9063d48e2a24b214b894b0e
Nour Elsayed <a_nah_123@hotmail.com>
2026-03-28 21:31:00 +0200

    created FeatureSection

---
+++ src/components/organisms/FeatureSection.tsx
@@ -0,0 +1,7 @@
+export default function FeatureSection() {
+  return (
+    <div className="bg-gray-400 text-white w-full p-5 flex justify-center items-center">
+     FeatureSection
+    </div>
+  );
+}
