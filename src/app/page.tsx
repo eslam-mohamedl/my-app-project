7a8619bd68a4d5ba0756a7ff772487cd31c7cc7e
Ahmed2005-CODE <ahmedkhalil555333@gmail.com>
2026-03-28 21:11:17 +0200

    Add Ahmed

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,12 @@
 import HeroSection from "@/components/organisms/HeroSection";
 import Navbar from "@/components/organisms/Navbar";
+import Ahmed from "@/components/Ahmed";
 export default function page() {
   return (
     <div>
       <HeroSection />
       <Navbar />
+      <Ahmed/>
     </div>
   );
 }
