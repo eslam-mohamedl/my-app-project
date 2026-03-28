5dbab25fc0eb7cfee4189bb43c08fe0456f5099b
mahmoud <mahmoudshalaby363@gmail.com>
2026-03-28 21:05:28 +0200

    feat/add Testimonial Section

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,12 @@
 import HeroSection from "@/components/organisms/HeroSection";
 import Navbar from "@/components/organisms/Navbar";
+import Testimonial from "@/components/organisms/Testimonial";
 export default function page() {
   return (
     <div>
       <HeroSection />
       <Navbar />
+      <Testimonial />
     </div>
   );
 }
