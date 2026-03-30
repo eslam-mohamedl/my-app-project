9230157c8b035fad96611c8262214441d5e0d2cf
Reham-Abdelaleem <rehamabdelalem08@gmail.com>
2026-03-30 10:04:45 +0200

    add PartnersSection

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,8 +1,10 @@
 import SkillsSection from "@/components/organisms/SkillsSection"
+import PartnersSection from "@/components/organisms/PartnersSection"
 export default function page() {
   return (
     <div>
       <SkillsSection />
+      <PartnersSection />
     </div>
   );
 }
