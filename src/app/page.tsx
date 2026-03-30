1ac6696971252bb78d170aa1128ca77ac26edb02
Mohamed Mostafa <mohamed188882838@gmail.com>
2026-03-30 03:11:46 +0200

    add mohamed3 components in page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -15,6 +15,7 @@ import AboutSection from "@/components/organisms/AboutSection";
 import GhezwanSection from "@/components/organisms/GhezwanSection";
 import Mohamed from "@/components/organisms/mohamed";
 import Mohamed2 from "@/components/organisms/mohamed2";
+import Mohamed3 from "@/components/organisms/Mohamed3";
 
 export default function page() {
   return (
@@ -36,6 +37,8 @@ export default function page() {
       <GhezwanSection />
        <Mohamed />
        <Mohamed2 />
+       <Mohamed3 />
+
     </div>
   );
 }
