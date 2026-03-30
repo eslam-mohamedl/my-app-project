27a3d5fc0fa617544215b00b3626f6680b9c5eeb
Reham-Abdelaleem <rehamabdelalem08@gmail.com>
2026-03-30 08:56:01 +0200

    update project

--- src/components/organisms/GhezwanSection.tsx
+++
@@ -1,7 +0,0 @@
-export default function GhezwanSection() {
-  return (
-    <div className="bg-amber-600 text-white w-full p-5 flex justify-center items-center">
-      GhezwanSection
-    </div>
-  );
-}
