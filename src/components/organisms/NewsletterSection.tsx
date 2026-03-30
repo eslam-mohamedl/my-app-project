96ccc385086882c037cf180d8040035cb938f80b
Reham-Abdelaleem <rehamabdelalem08@gmail.com>
2026-03-30 10:20:37 +0200

    update textcolor

--- src/components/organisms/NewsletterSection.tsx
+++ src/components/organisms/NewsletterSection.tsx
@@ -2,6 +2,6 @@ import React from 'react'
 
 export default function NewsletterSection() {
    return (
-      <div className='w-full p-3 text-white text-center font-2xl bg-gray-200'>Newsletter Section</div>
+      <div className='w-full p-3 text-black text-center font-2xl bg-gray-200'>Newsletter Section</div>
    )
 }
